9afaf787a59ec3fab8dd41bd104ec44a41fe9e3c
Yogeswaran-M <yogeglceo@gmail.com>
2025-10-10 19:47:10 +0530

    route structure

--- main.js
+++ main.js
@@ -11,3 +11,28 @@ app.listen(PORT, () => {
     console.log(`This server is running at http://localhost:${PORT}`);
     
 });
+
+// CRUD functionality of movies
+
+// R -for reading
+
+app.get("/movies", () => {
+
+})
+
+//C -for create
+
+app.post("/movies", () => {
+
+})
+
+//U -for update
+
+app.put("/movies/:id", () => {
+
+})
+
+//D -for delete
+app.delete("/movies/:id", () => {
+
+})
